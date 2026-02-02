f5dd01a087074d1ef2b68f0b85772077b498acc2
David Lawrence <dkl@mozilla.com>
2026-02-01 23:41:14 -0500

    Review comments from pull request #8

--- test_formatting.py
+++ test_formatting.py
@@ -6,11 +6,11 @@ import subprocess
 
 
 def test_black():
-    cmd = ("black", "--diff", "main.py")
+    cmd = ("black", "--diff", ".")
     output = subprocess.check_output(cmd)
     assert not output, "The python code does not adhere to the project style."
 
 
 def test_ruff():
-    passed = subprocess.call(("ruff", "check", "main.py", "--target-version", "py314"))
+    passed = subprocess.call(("ruff", "check", "."))
     assert not passed, "ruff did not run cleanly."
