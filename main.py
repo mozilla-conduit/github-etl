50c1ed09e2927106cd94f218340a13a7957972a8
David Lawrence <dkl@mozilla.com>
2026-02-24 16:31:25 -0500

    Fixed formatting

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def get_installation_access_token(
     headers = {
         "Authorization": f"Bearer {jwt}",
         "Accept": "application/vnd.github+json",
-        "X-GitHub-Api-Version": "2022-11-28"
+        "X-GitHub-Api-Version": "2022-11-28",
     }
 
     installation_id = _repo_installation_cache.get(repo)
